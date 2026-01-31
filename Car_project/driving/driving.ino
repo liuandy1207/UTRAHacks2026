eed47a263d3fed0634ea132ec061b91d43b6d467
Kevin Li <kevinsouthmarch@gmail.com>
2026-01-31 15:29:46 -0500

    Drive functions finished

--- Car_project/driving/driving.ino
+++ Car_project/driving/driving.ino
@@ -1,39 +1,68 @@
-// Motor pin definitions for L293D/L298N
-#define MOTOR1_ENA 9   // PWM pin for speed control
-#define MOTOR1_IN1 8   // Direction control
-#define MOTOR1_IN2 7   // Direction control
+// Motor pin definitions 
+#define MOTOR_R_IN1 1   // Direction control
+#define MOTOR_R_IN2 2   // Direction control
+
+#define MOTOR_L_IN3 3   // Direction control
+#define MOTOR_L_IN4 4   // Direction control
 
 void setupMotor() {
-  pinMode(MOTOR1_ENA, OUTPUT);
-  pinMode(MOTOR1_IN1, OUTPUT);
-  pinMode(MOTOR1_IN2, OUTPUT);
+    pinMode(MOTOR_R_IN1, OUTPUT);
+    pinMode(MOTOR_R_IN2, OUTPUT);
+    pinMode(MOTOR_L_IN3, OUTPUT);
+    pinMode(MOTOR_L_IN4, OUTPUT);
 }
 
 // Function with more control
-void controlMotor(int speed, int direction) {
-  // direction: 0=stop, 1=forward, -1=reverse
-  speed = abs(speed); // Ensure speed is positive
-  speed = constrain(speed, 0, 255);
-  
-  switch(direction) {
-    case 1: // Forward
-      digitalWrite(MOTOR1_IN1, HIGH);
-      digitalWrite(MOTOR1_IN2, LOW);
-      analogWrite(MOTOR1_ENA, speed);
-      break;
-      
-    case -1: // Reverse
-      digitalWrite(MOTOR1_IN1, LOW);
-      digitalWrite(MOTOR1_IN2, HIGH);
-      analogWrite(MOTOR1_ENA, speed);
-      break;
-      
-    case 0: // Stop
-    default:
-      digitalWrite(MOTOR1_IN1, LOW);
-      digitalWrite(MOTOR1_IN2, LOW);
-      analogWrite(MOTOR1_ENA, 0);
-      break;
-  }
+void drive() {
+    // Move forward
+    digitalWrite(MOTOR_R_IN1, HIGH);
+    digitalWrite(MOTOR_R_IN2, LOW);
+    
+    digitalWrite(MOTOR_L_IN3, HIGH);
+    digitalWrite(MOTOR_L_IN4, LOW);
+}
+
+void reverse() {
+    // Move backward
+    digitalWrite(MOTOR_R_IN1, LOW);
+    digitalWrite(MOTOR_R_IN2, HIGH);
+    
+    digitalWrite(MOTOR_L_IN3, LOW);
+    digitalWrite(MOTOR_L_IN4, HIGH);
+}
+
+void stop() {
+    // Stop motors
+    digitalWrite(MOTOR_R_IN1, LOW);
+    digitalWrite(MOTOR_R_IN2, LOW);
+    
+    digitalWrite(MOTOR_L_IN3, LOW);
+    digitalWrite(MOTOR_L_IN4, LOW);
+}
+
+void brake() {
+    // Brake motors
+    digitalWrite(MOTOR_R_IN1, HIGH);
+    digitalWrite(MOTOR_R_IN2, HIGH);
+    
+    digitalWrite(MOTOR_L_IN3, HIGH);
+    digitalWrite(MOTOR_L_IN4, HIGH);
+}
+void turnLeft() {
+    // Implementation for turning left
+    digitalWrite(MOTOR_R_IN1, HIGH);
+    digitalWrite(MOTOR_R_IN2, LOW);
+    
+    digitalWrite(MOTOR_L_IN3, LOW);
+    digitalWrite(MOTOR_L_IN4, HIGH);
+}
+
+void turnRight() {
+    // Implementation for turning right
+    digitalWrite(MOTOR_R_IN1, LOW);
+    digitalWrite(MOTOR_R_IN2, HIGH);
+
+    digitalWrite(MOTOR_L_IN3, HIGH);
+    digitalWrite(MOTOR_L_IN4, LOW);
 }
 
