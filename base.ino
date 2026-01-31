8c9c64176fd08488b815201d9e46c528dadf4c5b
He Ming (Andy) Liu <liuandy1207@gmail.com>
2026-01-31 18:08:02 -0500

    Update base.ino

--- base.ino
+++ base.ino
@@ -1,20 +1,25 @@
 // Servo
 #include <Servo.h>
 Servo servo;
-#define servoPin = 7;
+#define servoPin 7
 
 // Left Motor
-#define IN1 = 2;
-#define IN2 = 3;
+#define IN1 2
+#define IN2 3
 
 // Right Motor
-#define IN3 = 4;
-#define IN4 = 5;
+#define IN3 4
+#define IN4 5
 
 // IR Sensors
-#define IR1 = 8;
+#define IR1 8
 
 // Color Sensor
+#define S0 13
+#define S1 12
+#define S2 11
+#define S3 10
+#define OUT 9
 
 void setup() {
   Serial.begin(9600);
@@ -30,10 +35,20 @@ void setup() {
 
   // Servo Setup
   servo.attach(servoPin);
+
+  // Color Sensor Setup
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(OUT, INPUT);
+  digitalWrite(S0, HIGH); // set frequency scaling
+  digitalWrite(S1, LOW);  
 }
 
 void loop() {
-
+  identifyColor();
+  delay(500);
 }
 
 // Servo Functions
@@ -43,6 +58,55 @@ void rotate(int deg) {
 
 // Color Sensor Functions
 
+// Calibration Values
+int redMin = 25; 
+int redMax = 267; 
+int greenMin = 26; 
+int greenMax = 231; 
+int blueMin = 20; 
+int blueMax = 216; 
+
+int identifyColor() {
+  int r = getAverage(LOW, LOW);
+  int g = getAverage(HIGH, HIGH);
+  int b = getAverage(LOW, HIGH);
+
+  r = constrain(map(r, redMin, redMax, 255, 0), 0, 255);
+  g = constrain(map(g, greenMin, greenMax, 255, 0), 0, 255);
+  b = constrain(map(b, blueMin, blueMax, 255, 0), 0, 255);
+  
+  Serial.print("R: "); Serial.print(r);
+  Serial.print(" G: "); Serial.print(g);
+  Serial.print(" B: "); Serial.print(b);
+
+  Serial.print(" - Color: ");
+  if (r < 50 && g < 50 && b < 50) {
+    Serial.println("BLACK");
+  } else if (r > 200 && g > 200 && b > 200) {
+    Serial.println("WHITE");
+  } else if (r > g && r > b) {
+    Serial.println("RED");
+  } else if (g > r && g > b) {
+    Serial.println("GREEN");
+  } else if (b > r && b > g) {
+    Serial.println("BLUE");
+  } else {
+    Serial.println("UNCERTAIN");
+  }
+  return 0;
+}
+
+int getAverage(int s2State, int s3State) {
+  int sum = 0;
+  int readings = 50;
+
+  digitalWrite(S2, s2State);
+  digitalWrite(S3, s3State);
+  for (int i = 0; i < readings; i++) {
+    sum += pulseIn(OUT, LOW);
+  }
+  return sum / readings;
+}
 
 
 // IR Functions
