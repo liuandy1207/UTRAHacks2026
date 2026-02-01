549a9d0e79fc8084dfe0f790d9e4fc07f55dc8b2
Kevin Li <kevinsouthmarch@gmail.com>
2026-01-31 20:17:46 -0500

    Testing Changes

--- Car_project/segment_1/segment_1.ino
+++ Car_project/segment_1/segment_1.ino
@@ -6,21 +6,7 @@
 
 // Follow Black Line
 
-// Look for blue color to stop
+// look for red line 
 
-// Pick up cube (right side)
-
-// return to line 
-
-// Follow black 
-
-// Identify split
-
-// Identify Red 
-
-// Take right red 
-
-// Look for blue to stop
-
-// drop cube (left side)
+// look 
 
--- Car_project/segment_test/segment_test.ino
+++ Car_project/segment_test/segment_test.ino
@@ -1,18 +1,18 @@
 // Servo
 #include <Servo.h>
 Servo servo;
-#define servoPin = 7;
+#define servoPin 7
 
 // Left Motor
-#define IN1 = 2;
-#define IN2 = 3;
+#define IN1 2
+#define IN2 3
 
 // Right Motor
-#define IN3 = 4;
-#define IN4 = 5;
+#define IN3 4
+#define IN4 5
 
 // IR Sensors
-#define IR1 = 8;
+#define IR1 8
 
 // Color Sensor
 
@@ -32,9 +32,6 @@ void setup() {
   servo.attach(servoPin);
 }
 
-void loop() {
-
-}
 
 // Servo Functions
 void rotate(int deg) {
@@ -59,14 +56,14 @@ void readIR(int pin) {
 }
 
 // Motor Functions
-void forward() {
+void backward() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
 }
 
-void backward() {
+void forward() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
@@ -80,14 +77,14 @@ void brake() {
   digitalWrite(IN4, HIGH);
 }
 
-void turnLeft() {
+void turnRight() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
 }
 
-void turnRight() {
+void turnLeft() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH);
@@ -96,5 +93,34 @@ void turnRight() {
 
 
 void loop() {
-  
+
+  delay(2000);
+  Serial.println("Rotate Servo to 0");
+  rotate(0); // low
+  delay(1000);
+
+  forward();
+  delay(2000);
+  brake();
+
+  Serial.println("Rotate Servo to 90");
+  rotate(90); // high
+
+  backward();
+  delay(1000);
+  brake();
+
+  forward();
+  delay(1000);
+  brake();
+
+  rotate(0); // low
+  delay(1000);
+
+  backward();
+  delay(1000);
+
+  brake();
+
+  Serial.println("End of Test");
 }
