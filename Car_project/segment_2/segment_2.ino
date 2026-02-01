15c76bd62b2a0c5cc6f7ef56bfade9d127b02519
Kevin Li <kevinsouthmarch@gmail.com>
2026-02-01 07:13:41 -0500

    segment 2

--- Car_project/segment_2/segment_2.ino
+++ Car_project/segment_2/segment_2.ino
@@ -1,5 +1,380 @@
 // paste the base.ino here
 
+// Servo
+#include <Servo.h>
+Servo servo;
+#define servoPin 7
+
+// Left Motor
+#define IN1 2
+#define IN2 3
+
+// Right Motor
+#define IN3 4
+#define IN4 5
+
+// Echo Sensor
+#define TRIG 8
+#define ECHO 6
+
+// Color Sensor
+#define S0 13
+#define S1 12
+#define S2 11
+#define S3 10
+#define OUT 9
+
+// Calibration Values
+int redMin = 18,  redMax = 26;
+int greenMin = 13, greenMax = 33;
+int blueMin = 13, blueMax = 28;
+
+// Calibration Values 2
+int redMin2 = 12,  redMax2 = 28;
+int greenMin2 = 20, greenMax2 = 26;
+int blueMin2 = 19, blueMax2 = 30;
+
+void setup() {
+  Serial.begin(115200);
+
+  // Motor Setup
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  // Servo Setup
+  servo.attach(servoPin);
+
+  // Color Sensor Setup
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(OUT, INPUT);
+  digitalWrite(S0, HIGH); // set frequency scaling
+  digitalWrite(S1, LOW);  
+
+  // Echo Sensor Setup
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+  servo.write(90);
+}
+
+// Servo Functions
+void rotate(int deg) {
+  servo.write(deg);
+  // 0 puts the claw low, back leg high
+  // 90 puts the claw high, back leg low
+}
+
+// Color Sensor Functions
+int identifyColor() {
+  int r = getAverage(LOW, LOW);
+  int g = getAverage(HIGH, HIGH);
+  int b = getAverage(LOW, HIGH);
+
+  r = constrain(map(r, redMin, redMax, 255, 0), 0, 255);
+  g = constrain(map(g, greenMin, greenMax, 255, 0), 0, 255);
+  b = constrain(map(b, blueMin, blueMax, 255, 0), 0, 255);
+  
+  Serial.print("R: "); Serial.print(r);
+  Serial.print(" G: "); Serial.print(g);
+  Serial.print(" B: "); Serial.print(b);
+
+  Serial.print(" - Color: ");
+  if (r < 50 && g < 50 && b < 50) {
+    Serial.println("BLACK");
+    return 4;
+  } else if (r > 200 && g > 200 && b > 200) {
+    Serial.println("WHITE");
+    return 0;
+  } else if (r > g && r > b) {
+    Serial.println("RED/BROWN");
+    return 1;
+  } else if (g > r && g > b) {
+    Serial.println("GREEN");
+    return 2;
+  } else if (b > r && b > g) {
+    Serial.println("BLUE");
+    return 3;
+  } else {
+    Serial.println("UNCERTAIN");
+  }
+  return 0;
+}
+
+// Use for elevated!!!
+int identifyColor2() {
+  int r = getAverage(LOW, LOW);
+  int g = getAverage(HIGH, HIGH);
+  int b = getAverage(LOW, HIGH);
+
+  r = constrain(map(r, redMin2, redMax2, 255, 0), 0, 255);
+  g = constrain(map(g, greenMin2, greenMax2, 255, 0), 0, 255);
+  b = constrain(map(b, blueMin2, blueMax2, 255, 0), 0, 255);
+  
+  Serial.print("R: "); Serial.print(r);
+  Serial.print(" G: "); Serial.print(g);
+  Serial.print(" B: "); Serial.print(b);
+
+  Serial.print(" - Color: ");
+  if (r < 50 && g < 50 && b < 50) {
+    Serial.println("BLACK");
+    return 4;
+  } else if (r > 180 && g > 180 && b > 180) {
+    Serial.println("WHITE");
+    return 0;
+  } else if (r > g && r > b) {
+    Serial.println("RED/BROWN");
+    return 1;
+  } else if (g > r && g > b) {
+    Serial.println("GREEN");
+    return 2;
+  } else if (b > r && b > g) {
+    Serial.println("BLUE");
+    return 3;
+  } else {
+    Serial.println("UNCERTAIN");
+  }
+  return 0;
+}
+
+int getAverage(int s2State, int s3State) {
+  int sum = 0;
+  int readings = 50;
+
+  digitalWrite(S2, s2State);
+  digitalWrite(S3, s3State);
+  for (int i = 0; i < readings; i++) {
+    sum += pulseIn(OUT, LOW);
+  }
+  return sum / readings;
+}
+
+// Echo Sensor Code
+int checkDistance() {
+  // clear it
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  // get the reading
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  long duration = pulseIn(ECHO, HIGH);
+  float distance = duration * 0.034 / 2;
+  return distance;
+}
+
+/* IR Functions
+void readIR(int pin) {
+  int sum = 0;
+  int readings = 10;
+  for (int i = 0; i < readings; i++) {
+    sum += digitalRead(pin);
+    delay(5);
+  }
+  int state = (sum > readings/2) ? HIGH : LOW;
+  Serial.println(state ? "Object Detected!" : "Naw...");
+  delay(100);
+} 
+*/
+
+// Motor Functions
+void backward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void forward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void brake() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);
+}
+
+void turnLeft() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void turnRight() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+//new function
+void obstacle_avoidance_sequence() {
+
+  // 1. Follow red until obstacle (< 10 cm)
+  if (checkDistance() < 10) {
+
+
+    // 2. Turn right for 10 seconds
+    turnLeft();
+    delay(1900);
+    brake();
+
+    // 3. Check distance again
+    if (checkDistance() >= 10) {
+
+      // No obstacle
+      forward();
+      delay(3500);
+      brake();
+
+    } else {
+
+      // Obstacle still present
+      turnLeft();
+      delay(2000);
+      brake();
+
+      forward();
+      delay(3500);
+      brake();
+    }
+
+    // 4. Turn left for 10 seconds to parallel with red line
+    turnRight();
+    delay(1900);
+    brake();
+
+    forward();
+    delay(3500);
+    brake();
+
+    // 5. turn left for 10 seconds to face red line
+    turnRight();
+    delay(1900); 
+    brake();
+    
+    // 5. Move forward until object is at red, stopping to poll every 0.25s
+    while (identifyColor() != 1) {
+      forward();
+      delay(250);
+    }
+    brake();
+
+    // 6. Turn left for 10 seconds
+    turnLeft();
+    delay(1000);
+    brake();
+  }
+}
+
+//new function end
+void moveAndCheckColor(int tColour, char dir, int bg) {
+  bool begin = true;
+  int almost90 = 2000; // TODO: fix
+  int step = int(almost90/10);
+  int colour = identifyColor();
+  while(colour != 3 && colour != bg){ //3= brown
+    Serial.println("checked");
+    if (colour == tColour){
+      Serial.println("straight");
+      begin = true;
+      forward();
+      delay(500);
+    }
+    else if (begin){
+      Serial.println("command");
+
+      if(dir == 'l'){
+        turnLeft();//amount to get to just under 180deg left
+      }
+      else{
+        turnRight();//amount to get to just under 180deg left
+      }
+      delay(almost90); //dir
+      begin = false;
+    }
+    else{
+      if(dir == 'l'){
+        turnRight(); //dir
+      }
+      else{
+        turnLeft();
+      }
+      delay(step);
+    }
+    int dist = checkDistance();
+    if (dist < 20){
+      return;
+    }
+    colour = identifyColor();
+  }
+}
+void findBall(){
+  turnLeft();
+  int almost90 = 2500; 
+  delay(almost90);
+  int minDist = 1000;
+  int totalTurn = 0;
+  while(totalTurn < almost90*2){
+    
+  }
+  //lowly turn right
+}
+
+
+void moveAndCheckColor2(int tColour, char dir, int bg) {
+  bool begin = true;
+  int almost90 = 2000; // TODO: fix
+  int step = int(almost90/10);
+  int colour = identifyColor2();
+  while(colour != 3 && colour != bg){ //3= brown
+    Serial.println("checked");
+    if (colour == tColour){
+      Serial.println("straight");
+      begin = true;
+      forward();
+      delay(500);
+    }
+    else if (begin){
+      Serial.println("command");
+
+      if(dir == 'l'){
+        turnLeft();//amount to get to just under 180deg left
+      }
+      else{
+        turnRight();//amount to get to just under 180deg left
+      }
+      delay(almost90); //dir
+      begin = false;
+    }
+    else{
+      if(dir == 'l'){
+        turnRight(); //dir
+      }
+      else{
+        turnLeft();
+      }
+      delay(step);
+    }
+    int dist = checkDistance();
+    if (dist < 20){
+      return;
+    }
+    colour = identifyColor2();
+  }
+}
+
+
 // Run path finding function (red line detection)
     // should reach obstacle avoidance sequence
     // return to red line 
