5d2821f57a84e8f246ebbf2179fe8658e1ebead1
He Ming (Andy) Liu <liuandy1207@gmail.com>
2026-02-01 01:23:38 -0500

    Update segment_3.ino

---
+++ Car_project/segment_3/segment_3.ino
@@ -0,0 +1,8 @@
+// circle
+
+// start in circle, need to locate middle black square
+
+// on the spot radar check for minimum distance (would be perp to wall, so centered)
+
+
+// push / poll towards middle, check for black, then blue
